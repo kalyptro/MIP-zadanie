a77770b327f383475931548b845fe3c5cbf38980
kalyptro <samuel.rosolanka@gmail.com>
2018-11-26 18:22:44 +0100

    added basic structure

--- Source.c
+++ Source.c
@@ -2,7 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct zaznamy{
+	char kategoria[50];
+	char znak[50];
+	char predajca[50];
+	int cena;
+	int rok_vyroby;
+	char stav_vozidla[200];
+	struct zaznamy *next;
+} ZAZNAM;
+
 int main() {
+
 
 	return 0;
 }
